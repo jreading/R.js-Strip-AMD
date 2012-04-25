30a56f1aed5f76779e1bae835c7f2c5071be6640
John Reading <john@resonancemultimedia.com>
2012-04-25 10:59:46 -0400

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ R.js-Strip-AMD
 ==============
 
 An R.js onBuildWrite for stripping AMD ceremony
+
+Add this to your build.js and it will remove all AMD wrappers for a clean, concatenated, and optimized file that does not require client-side loading of require.js or almond.js.
+
---
+++ build-strip-amd.js
@@ -0,0 +1,11 @@
+({
+	onBuildWrite: function (id, path, contents) {
+		var defineRegExp = /define\s*\(\s*["'][^'"]+["']\s*,\s*\[[^\]]*\]\s*,function\s*?\(.*?\)\s*?\{/;
+        //Remove AMD ceremony for use without require.js or almond.js
+        contents =  contents.replace(defineRegExp, '')
+                    //Remove the trailing }) for the define call
+                    //and any semicolon
+                    .replace(/\}\)(;)?\s*$/, '');
+        return contents;
+    }
+})
