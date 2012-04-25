13224ea9f41420037a78a5a4e928a8e133b9a112
John Reading <john@resonancemultimedia.com>
2012-04-25 11:21:58 -0400

    tab/spaces

--- build-strip-amd.js
+++ build-strip-amd.js
@@ -2,10 +2,9 @@
 	onBuildWrite: function (id, path, contents) {
 		var defineRegExp = /define\s*\(\s*["'][^'"]+["']\s*,\s*\[[^\]]*\]\s*,function\s*?\(.*?\)\s*?\{/;
         //Remove AMD ceremony for use without require.js or almond.js
-        contents =  contents.replace(defineRegExp, '')
-                    //Remove the trailing }) for the define call
-                    //and any semicolon
-                    .replace(/\}\)(;)?\s*$/, '');
-        return contents;
-    }
+		contents =  contents.replace(defineRegExp, '')
+		//Remove the trailing }) for the define call and any semicolon
+		.replace(/\}\)(;)?\s*$/, '');
+		return contents;
+	}
 })
