46e44dfa01d318ea666a9c562c4920f18d6aee7a
John Reading <john@resonancemultimedia.com>
2012-05-08 12:04:50 -0300

    radically simplified regex

--- build-strip-amd.js
+++ build-strip-amd.js
@@ -1,6 +1,6 @@
 ({
 	onBuildWrite: function (id, path, contents) {
-		var defineRegExp = /define\s*\(\s*["'][^'"]+["']\s*,\s*\[[^\]]*\]\s*,function\s*?\(.*?\)\s*?\{/;
+		var defineRegExp = /define.*?{/;
 		//Remove AMD ceremony for use without require.js or almond.js
 		contents =  contents.replace(defineRegExp, '')
 		//Remove the trailing }) for the define call and any semicolon
